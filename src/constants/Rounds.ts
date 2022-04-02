a966c36719977ee62282262fd8f7426c7d89130b
Mészáros Martin <martin44548@gmail.com>
2022-04-02 19:10:29 +0200

    change rounds time

--- src/constants/Rounds.ts
+++ src/constants/Rounds.ts
@@ -4,131 +4,130 @@ export const ROUNDS: IRound[] = [
 	{
 		round: 1,
 		bigBlind: 200,
-		duration: 15
+		duration: 10
 	},
 	{
 		round: 2,
 		bigBlind: 400,
-		duration: 15
+		duration: 10
 	},
 	{
 		round: 3,
 		bigBlind: 600,
-		duration: 15
+		duration: 10
 	},
 	{
 		round: 4,
 		bigBlind: 800,
-		duration: 15
+		duration: 10
 	},
-	// 1 óra
 	{
 		round: 5,
 		bigBlind: 1000,
-		duration: 15
+		duration: 10
 	},
 	{
 		round: 6,
 		bigBlind: 1200,
-		duration: 15
+		duration: 10
 	},
+	// 1 óra
 	{
 		round: 7,
 		bigBlind: 1400,
-		duration: 15
+		duration: 10
 	},
 	{
 		round: 8,
 		bigBlind: 1600,
-		duration: 15
+		duration: 10
 	},
 	{
 		round: 9,
-		bigBlind: 1600,
-		duration: 15
+		bigBlind: 1800,
+		duration: 10
 	},
-	// 2 óra
 	{
 		round: 10,
-		bigBlind: 1800,
-		duration: 12
+		bigBlind: 2000,
+		duration: 10
 	},
 	{
 		round: 11,
-		bigBlind: 2000,
-		duration: 12
+		bigBlind: 2400,
+		duration: 10
 	},
 	{
 		round: 12,
-		bigBlind: 2400,
-		duration: 12
+		bigBlind: 2700,
+		duration: 10
 	},
+	// 2 óra
 	{
 		round: 13,
-		bigBlind: 2800,
-		duration: 12
+		bigBlind: 3000,
+		duration: 10
 	},
 	{
 		round: 14,
 		bigBlind: 3400,
-		duration: 12
+		duration: 10
 	},
-	// 3 óra
 	{
 		round: 15,
-		bigBlind: 3800,
-		duration: 12
+		bigBlind: 4000,
+		duration: 10
 	},
 	{
 		round: 16,
-		bigBlind: 4000,
-		duration: 12
+		bigBlind: 4400,
+		duration: 10
 	},
 	{
 		round: 17,
-		bigBlind: 4400,
-		duration: 12
+		bigBlind: 4700,
+		duration: 10
 	},
 	{
 		round: 18,
-		bigBlind: 4400,
-		duration: 12
+		bigBlind: 5000,
+		duration: 10
 	},
+	// 3 óra
 	{
 		round: 19,
-		bigBlind: 4800,
-		duration: 12
-	},
-	// 4 óra
-	{
-		round: 20,
 		bigBlind: 5400,
 		duration: 10
 	},
 	{
-		round: 21,
+		round: 20,
 		bigBlind: 6000,
 		duration: 10
 	},
 	{
-		round: 22,
+		round: 21,
 		bigBlind: 7000,
 		duration: 10
 	},
 	{
-		round: 23,
+		round: 22,
 		bigBlind: 8000,
 		duration: 10
 	},
 	{
-		round: 24,
+		round: 23,
 		bigBlind: 9000,
-		duration: 10
+		duration: 8
+	},
+	{
+		round: 24,
+		bigBlind: 10000,
+		duration: 8
 	},
 	{
 		round: 25,
-		bigBlind: 10000,
-		duration: 10
+		bigBlind: 12000,
+		duration: 8
 	}
-	// 5 óra
+	// 4 óra
 ];
